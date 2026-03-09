060434ff2ff87ca88470f4c423207405b0222e71
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-03-08 23:52:54 -0700

    Update pitcher_game_card.py

--- pitcher_game_card.py
+++ pitcher_game_card.py
@@ -665,7 +665,7 @@ def game_line(game_id,pitcher_id):
 def load_prev_pitches(pitcher_id,game_id=None,prev_season=None,comp_year=None):
     data_load = []
     pitcher_height = player_height(pitcher_id)
-    if (prev_season) & (comp_year):
+    if prev_season:
         game_list = szn_games(pitcher_id,game_id,season=comp_year)
     else:
         if not game_id:
@@ -2238,8 +2238,6 @@ with col3:
         st.selectbox('Choose a pitcher:',list(pitcher_list.keys()),key='pitcher')
         pitcher_id = int(pitcher_list[ss['pitcher']][0])
         year_diff = 1
-        vs_past = False
-        comp_year = None
         while year_diff < 4:
             response = requests.get(url=f'http://statsapi.mlb.com/api/v1/people/{pitcher_id}?hydrate=stats(group=pitching,type=gameLog,season={ss['date'].year - year_diff},endDate={ss['date']},sportId=[1,51],gameType=[E,R,S]),hydrations').json()
             if 'stats' in response['people'][0].keys():
@@ -2247,6 +2245,9 @@ with col3:
                 comp_year = ss['date'].year - year_diff
                 break
             year_diff += 1
+        else:
+            vs_past = False
+            comp_year = None
             
         spring_training = True if requests.get(f"https://statsapi.mlb.com/api/v1.1/game/{game_id}/feed/live").json()['gameData']['game']['type'] in ['E','F','S'] else False
         # spring_training = st.checkbox("Is Spring Training Game?",value=True)
