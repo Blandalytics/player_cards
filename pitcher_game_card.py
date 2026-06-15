51d8f79e0c039c3cad0d968a05ff829c5c840739
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-06-14 23:04:02 -0700

    Update pitcher_game_card.py

--- pitcher_game_card.py
+++ pitcher_game_card.py
@@ -36,7 +36,7 @@ base_font = 'DM Sans'
 font = load_google_font(base_font, weight='bold')
 # set_default_font(font)
 italic = load_google_font(base_font, weight='bold', italic=True)
-fm.fontManager.addfont(str(font.get_file()))
+# fm.fontManager.addfont(str(font.get_file()))
 
 ## Set Styling
 # Plot Style
